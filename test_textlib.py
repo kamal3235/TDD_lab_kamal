eef727e7f14b2cf1869f67c8f6b48ec65bc9cd49
Abu H Kamal <kamal3235@hotmail.com>
2024-11-25 18:49:44 -0600

    after application code

--- test_textlib.py
+++ test_textlib.py
@@ -2,13 +2,25 @@ import unittest
 from textlib import BodyOfText
 
 class TestBodyOfText(unittest.TestCase):
+    # In "test_empty_story", create a BodyOfText instance with an empty
+    # string, and assert that its "num_paragraphs" method returns zero
     def test_empty_story(self):
         text = BodyOfText("")
         self.assertEqual(0, text.num_paragraphs())
 
+    # Add a method named "test_single_paragraph". Within it, create a
+    #    BodyOfText instance for a single paragraph (i.e., a string with no
+    #    blank lines, as described above). A single sentence will do fine.
+    #    Assert that ".num_paragraphs()" returns 1
+
     def test_single_paragraph(self):
         text = BodyOfText("A single sentence will do fine")
         self.assertEqual(1, text.num_paragraphs())
+
+    # Create a test string with at least three paragraphs (see example
+    #   below). Add a function named "test_several_paragraphs", which
+    #   creates a "BodyOfText" instance using that test string, then asserts
+    #   that ".num_paragraphs()" returns the correct number
 
     def test_several_paragraphs(self):
         text = BodyOfText("""This is a longer story. It has several paragraphs.
@@ -21,8 +33,14 @@ taken care of well by his parents.
 One day, the young dragon's wings finally grew big enough that he
 could start to fly. What a wonderful experience! For the first time in
 his life, he could gracefully sail over the tops of trees.
-        
+
 four paragraphs, End pf story""")
         self.assertEqual(4, text.num_paragraphs())
 
+# STEP 2 - NOT modifying "textlib.py
+# In Command line Identify following
+# Test module - in line of FAIL see test_textlib
+# test Class - in line of FAIL see TestBodyOf Text
+# test methods - in line of FAIL see test_empty_story
+# line number - Traceback 3rd line show Line 7, 26, 11
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
--- textlib.py
+++ textlib.py
@@ -1,7 +1,14 @@
+# Abu Kamal
+# Count paragraph
+# Split text to make paragraph (\n)
+# paragraphs = text.split('\n')
+
+
 class BodyOfText:
     def __init__(self, text):
-        self.text = text
+        self.text = text.strip()
+        # strip leading/trailing whitespace
     def num_paragraphs(self):
-        return -1
+        return len([p for p in self.text.split('\n\n') if p.strip()])
 
 # Part of Powerful Python Academy. Copyright MigrateUp LLC. All rights reserved.
